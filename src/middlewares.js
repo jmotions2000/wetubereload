9bdd8c4c876839ce118dabc64d2bf16503535c65
r2chh <r2chh@naver.com>
2023-01-30 12:33:16 +0900

    S3 upload

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,19 @@
 import multer from "multer";
+import aws from "aws-sdk";
+import multerS3 from "multer-s3";
 
+const s3 = new aws.S3({
+  region: "ap-northeast-2",
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
+  //credentials 를 사용하지 않고 region 사용
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wetubereloaded",
+  acl: "public-read",
+});
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
@@ -27,10 +41,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
